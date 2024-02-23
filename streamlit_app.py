ad3928c509921190427b32dfdb67d74590465852
manasi-mali-ai <160024171+manasi-mali-ai@users.noreply.github.com>
2024-02-23 19:17:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def search_videos(query, max_results=5):
         return []
 
 # Streamlit App Layout
-st.title("FitPal: Interactive Fitness Coach")
+st.title("SelfFit: Personal Fitness Coach")
 
 # Input fields
 client_info = st.text_area("Talk with Fit", help="Enter your message here")
